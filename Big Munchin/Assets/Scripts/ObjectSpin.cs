c0e12c1606049fd8972ef35d236f96542d2adfc7
Jacques <jacquesgati@gmail.com>
2023-10-01 20:50:18 -0400

    Initial menu creation
    Created pause menu with all menu navigation functioning

---
+++ Big Munchin/Assets/Scripts/ObjectSpin.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpin : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    /*private GameObject item;*/
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(0f, 3f, 0f);
+    }
+}
---
+++ Big Munchin/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+
+    public GameObject pauseMenu;
+    void Start()
+    {
+        
+    }
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading Menu...");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+}
