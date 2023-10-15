fde4e05361ebb6c7165c87486801869a6e172a0b
tabbicat17 <tabbicat123@gmail.com>
2023-10-14 23:45:17 -0400

    nutrition mg
    more work with quality of life on slicing

--- Big Munchin/Assets/Scripts/NGcontroller.cs
+++ Big Munchin/Assets/Scripts/NGcontroller.cs
@@ -8,15 +8,19 @@ public class NGcontroller : MonoBehaviour
     int currentScreen = 0;
     public GameObject knife;
     public GameObject counter;
-    bool alreadyMade = false;
+    string currentMade;
 
     //gets the name of the selected button and instantiates that object
     public void createObject()
     {
         string foodItem = EventSystem.current.currentSelectedGameObject.name;
-        Instantiate(Resources.Load(foodItem), new Vector3(4.105f, 1.181f, 182.282f), Quaternion.identity);
-        counter.GetComponent<sliceObject>().getSliceObject(foodItem);
-        knife.SetActive(true);
+        if(foodItem != currentMade)
+        {
+            Instantiate(Resources.Load(foodItem), new Vector3(4.105f, 1.181f, 182.282f), Quaternion.identity);
+            counter.GetComponent<sliceObject>().getSliceObject(foodItem);
+            knife.SetActive(true);
+            currentMade = foodItem;
+        }
     }
 
     public void openRecipeBook()
--- Big Munchin/Assets/Scripts/sliceObject.cs
+++ Big Munchin/Assets/Scripts/sliceObject.cs
@@ -2,38 +2,72 @@ using EzySlice;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class sliceObject : MonoBehaviour
 {
-    public Transform planeObject;
+    Transform planeObject;
+    public Transform planeObjectSmall;
+    public Transform planeObjectMed;
+    public Transform planeObjectLarge;
     public GameObject sliceO;
     public Material middleGuts;
     string sliceOName;
+    int meatAmount = 0;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Slice(sliceO);
-        }
+         //if (Input.GetKeyDown(KeyCode.Space))
+         //{
+         //    Slice(sliceO);
+         //}
     }
+
     public void getSliceObject(string foodName)
     {
         sliceOName = foodName + "(Clone)";
         sliceO = GameObject.Find(sliceOName);
     }
 
+    public void slicePortionSmall()
+    {
+        Debug.Log("clicked");
+        planeObject = planeObjectSmall;
+        meatAmount = 1;
+        Slice(sliceO);
+    }
+    public void slicePortionMed()
+    {
+        planeObject = planeObjectMed;
+        meatAmount = 2;
+        Slice(sliceO);
+    }
+    public void slicePortionLarge()
+    {
+        planeObject = planeObjectLarge;
+        meatAmount = 3;
+        Slice(sliceO);
+    }
+
     public void Slice(GameObject o)
     {
         SlicedHull hull = o.Slice(planeObject.position, planeObject.up);
-        Debug.Log("name: " + sliceOName);
         if (hull != null)
         {
             GameObject upperHull = hull.CreateUpperHull(o, middleGuts);
+            hullSetups(upperHull);
             GameObject lowerHull = hull.CreateLowerHull(o, middleGuts);
+            hullSetups(lowerHull);
 
             Destroy(o);
         }
+    }
+
+    public void hullSetups(GameObject o)
+    {
+        //Rigidbody rb = o.AddComponent<Rigidbody>();
+        o.AddComponent<Rigidbody>();
+        MeshCollider collider = o.AddComponent<MeshCollider>();
+        collider.convex = true;
     }
 }
