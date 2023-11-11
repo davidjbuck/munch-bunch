50e5045f674511568ad37f3e9c23fa8722680efa
tabbicat17 <tabbicat123@gmail.com>
2023-11-11 16:47:10 -0500

    Final NMG Mechanics
    basics fleshed out for nmg

--- Big Munchin/Assets/Scripts/NGcontroller.cs
+++ Big Munchin/Assets/Scripts/NGcontroller.cs
@@ -26,32 +26,63 @@ public class NGcontroller : MonoBehaviour
     public GameObject meatParent;
     public GameObject carbParent;
     public GameObject vegParent;
+    public GameObject sumParent;
 
     public GameObject riceParent;
     public ParticleSystem ricePour;
     float fakeWeightCount = 0;
     public TextMeshProUGUI weightTxt;
 
+    string vegName;
+    public GameObject plate1;
+    public GameObject plate2;
+    public TextMeshProUGUI mostPlateWeightTxt;
+    public TextMeshProUGUI vegWeightTxt;
+
+    public TextMeshProUGUI meatSumTXT;
+    public TextMeshProUGUI carbSumTXT;
+    public TextMeshProUGUI vegSumTXT;
+    public TextMeshProUGUI calSumTXT;
+
     private void Update()
     {
-        //particle system timing
-        if (Input.GetMouseButton(0))
+        if(currentScreen == 1)
         {
-            if(riceParent.activeInHierarchy == true)
+            //particle system timing
+            if (Input.GetMouseButton(0))
             {
-                fakeWeightCount += Time.deltaTime*2;
-                weightTxt.text = ("Weight: " + fakeWeightCount + "lbs");
+                if (riceParent.activeInHierarchy == true)
+                {
+                    fakeWeightCount += Time.deltaTime * 2;
+                    weightTxt.text = ("Weight: " + fakeWeightCount + "lbs");
+                }
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                ricePour.Play();
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                ricePour.Stop();
+            }
+        }
+        else if (currentScreen == 2)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if(vegParent != null)
+                {
+                    Instantiate(Resources.Load(vegName), new Vector3(-397.18f, 31.625f, 60.49f), Quaternion.identity);
+                    vegWeight++;
+                    vegWeightTxt.text = "Vegetable Weight: " + vegWeight;
+                }
             }
         }
 
-
-        if (Input.GetMouseButtonDown(0))
+        if (currentScreen == 3)
         {
-            ricePour.Play();
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            ricePour.Stop();
+            Destroy(GameObject.Find("brocoli(Clone)"));
         }
     }
 
@@ -91,10 +122,34 @@ public class NGcontroller : MonoBehaviour
     }
     public void switchToVeg()
     {
+        carbWeight = fakeWeightCount;
+        carbCal = fakeWeightCount * 2;
         currentScreen = 2;
+        plate1.SetActive(true);
+        plate2.SetActive(true);
+        mostPlateWeightTxt.text = "Current Weight: " + (meatWeight + carbWeight);
         riceParent.SetActive(false);
         carbParent.SetActive(false);
         vegParent.SetActive(true);
+    }
+
+    public void switchToSummary()
+    {
+        currentScreen = 3;
+        plate1.SetActive(false);
+        plate2.SetActive(false);
+        vegParent.SetActive(false);
+        sumParent.SetActive(true);
+
+        meatSumTXT.text = "Meat Weight: " + meatWeight + "      Desired Weight: " + 1;
+        carbSumTXT.text = "Carb Weight: " + carbWeight + "      Desired Weight: " + 2;
+        vegSumTXT.text = "Vegetable Weight: " + vegWeight + "      Desired Weight: " + 3;
+        calSumTXT.text = "Total Calories: " + meatCal + carbCal + vegWeight;
+    }
+
+    public void setVegName()
+    {
+        vegName = EventSystem.current.currentSelectedGameObject.name;
     }
 
     public void openRecipeBook()
